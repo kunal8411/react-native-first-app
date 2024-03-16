4c9309a61fcab07f2dee45deb70d4283b5843d4a
Kunal Khairnar <kkhairnar@nexuscognitive.com>
2024-03-16 13:36:49 +0530

    React native code setup

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>My First React Native App</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View>
+        <Text
+          style={{
+            margin: "16",
+            borderColor: "red",
+            borderWidth: 1,
+            borderColor: "red",
+            borderRadius: 3,
+            margin:10,
+            padding:5
+          }}
+        >
+          Hello Kunal Khairnar
+        </Text>
+        <Button title="Click me" />
+      </View>
     </View>
   );
 }
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
