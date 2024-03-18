a88d3bb5de0508162ab17b19db9a2ef44cd6b0d6
Kunal Khairnar <kkhairnar@nexuscognitive.com>
2024-03-18 20:23:16 +0530

    Started building the textarea and button components, Button events are pending

--- App.js
+++ App.js
@@ -1,38 +1,52 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>My First React Native App</Text>
-        <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Enter goals here" />
+        <Button title="Add goal" />
       </View>
-      <View>
-        <Text
-          style={{
-            margin: "16",
-            borderColor: "red",
-            borderWidth: 1,
-            borderColor: "red",
-            borderRadius: 3,
-            margin:10,
-            padding:5
-          }}
-        >
-          Hello Kunal Khairnar
-        </Text>
-        <Button title="Click me" />
+      <View style={styles.goalsContainer}>
+        <Text style={styles.goalsTextContainer}>Entered goals are.....</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    padding: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    padding: 8,
+    marginRight: 8,
+  },
+  goalsContainer: {
+    flex: 4,
+  },
+  goalsTextContainer: {
+    marginTop: 10,
   },
 });
+
+// const styles = StyleSheet.create({
+//   container:{
+
+//   }
+// })
