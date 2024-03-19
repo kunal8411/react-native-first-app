6dbb96c161bb8a409061c6c483aebb5edb303109
Kunal Khairnar <kkhairnar@nexuscognitive.com>
2024-03-19 17:30:39 +0530

    27.Optimizing list to FlatList

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [coarseGoals, setCoarseGoals] = useState([]);
+  const [coarseInput, setCoarseInput] = useState("");
+  const addNewGoal = () => {
+    setCoarseGoals((prevState) => [
+      ...prevState,
+      { name: coarseInput, id: Math.random() },
+    ]);
+  };
+  const onChangCoarseInput = (inputText) => {
+    setCoarseInput(inputText);
+  };
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Enter goals here" />
-        <Button title="Add goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter goals here"
+          onChangeText={onChangCoarseInput}
+        />
+        <Button title="Add goal" onPress={addNewGoal} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text style={styles.goalsTextContainer}>Entered goals are.....</Text>
+        <FlatList
+          data={coarseGoals}
+          keyExtractor={(item, data) => {
+            return item.id;
+          }}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalsContainer}>
+                <Text style={styles.goalsTextContainer}>
+                  {itemData?.item?.name}
+                </Text>
+              </View>
+            );
+          }}
+        />
       </View>
     </View>
   );
@@ -20,6 +57,7 @@ const styles = StyleSheet.create({
     padding: 50,
     paddingHorizontal: 16,
     flex: 1,
+    height: 10,
   },
   inputContainer: {
     flex: 1,
@@ -42,11 +80,7 @@ const styles = StyleSheet.create({
   },
   goalsTextContainer: {
     marginTop: 10,
+    backgroundColor: "lightblue",
+    padding: 5,
   },
 });
-
-// const styles = StyleSheet.create({
-//   container:{
-
-//   }
-// })
